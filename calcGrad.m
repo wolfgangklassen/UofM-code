c937e26644028aac39c052441bbcde15e6cd2378
Wolfgang Klassen <wolfyklassen@gmail.com>
2019-01-07 16:50:10 -0600

    edited agin for practice

--- calcGrad.m
+++ calcGrad.m
@@ -1,7 +1,7 @@
 function g_fit = calcGrad(B,transMat)
 %calcGrad is essentially a wrapper around the "pinv" function, to
 %make my other code more readable.  This also allows modifications to be
-%made in the future to how this is done.
+%made in the future to how this is done. edit
 
 g_fit = pinv(transMat)*B;
 
