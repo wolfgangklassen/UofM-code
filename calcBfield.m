4cd9f8ebdddf06be8e771efd03c23912d893c37d
Wolfgang Klassen <wolfyklassen@gmail.com>
2019-01-08 10:30:23 -0600

    test edit from matlab

--- calcBfield.m
+++ calcBfield.m
@@ -3,7 +3,7 @@ function Bmod = calcBfield(transMat,g)
 %transformation matrices.  Then it calculates Bmod, which is then reported
 %as the output.  This value is later used as Bz for the purposes of fitting to
 %the harmonic functions. transMat is a vector of 3 transformation matrices, one for each
-%dimension
+%dimension another edit
 
 Bx = transMat(:,:,1)*g;
 By = transMat(:,:,2)*g;
