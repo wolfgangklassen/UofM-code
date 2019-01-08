e5be143dba2aaa75f3c928ae27b140605f41a2eb
Wolfgang Klassen <wolfyklassen@gmail.com>
2019-01-08 10:21:38 -0600

    changed test edits back

--- calcdFalseThird.m
+++ calcdFalseThird.m
@@ -4,7 +4,7 @@ function dFalse = calcdFalseThird(R,h1,h2,g)
 %origin, and total height (h2-h1) in cm, and given the gradient vector g.
 %This calculation assumes the gradient vector is given in units of 
 %microtesla/cm^{ell}, where ell is the degree of the polynomial associated
-%with the gradient term.  This version calculates up to third order. edit
+%with the gradient term.  This version calculates up to third order.
 
 dFalse = 8e-30*(R^2/4)*(g(6)*1e6 + g(12)*1e6*(h2+h1) - (g(20)*1e6/2)*(R^2 - 2*h1^2 - 2*h1*h2 - 2*h2^2));
 
