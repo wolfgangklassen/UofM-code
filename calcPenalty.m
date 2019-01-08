1721cb9bb6b6a5926c72b8423a0ca0fc80bb68d7
Wolfgang Klassen <wolfyklassen@gmail.com>
2019-01-08 13:02:11 -0600

    first draft done for isPenalty

--- calcPenalty.m
+++ calcPenalty.m
@@ -4,9 +4,6 @@ penalty = 0;
 x = 0;
 for i = 1:length(pos(:,1))
     
-    r = sqrt(pos(i,1).^2 + pos(i,2).^2 + pos(i,3).^2);
-    rho = sqrt(pos(i,1).^2 + pos(i,2).^2);
-    z = pos(i,3);
     
     if isequal([z>extPar.H,rho<extPar.R2],[1,1])
         x = z - extPar.H;
@@ -16,9 +13,9 @@ for i = 1:length(pos(:,1))
         x = r - sqrt(extPar.R2.^2 + extPar.H.^2);
     elseif isequal([z<extPar.h2,rho<extPar.R],[1,1])
         if z < rho
-            x = (extPar.R./rho).^5 - 1;
+            x = (extPar.R./rho).^10 - 1;
         else
-            x = (extPar.h2/z).^5 - 1;
+            x = (extPar.h2/z).^10 - 1;
         end
     end
     
