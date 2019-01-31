758d70bd6c7acf8f3ce69f0314d364ee32bd7415
Wolfgang <wolfyklassen@gmail.com>
2019-01-31 11:00:42 -0600

    added averaging over different g vectors

--- calcFitness.m
+++ calcFitness.m
@@ -2,23 +2,18 @@ function fitness = calcFitness(Xtemp,extPar,model)
 %calcFitness takes in a position ad the simulated g values, and reports how
 %well the program was able to extract the gradients with the fit function
 
-fitness = 0;
-pos = reshape(Xtemp,length(Xtemp)./3,3);
-penalty = 0;
-auxOutput(model).penalty = false;
-for i = 1:length(pos(:,1))
-    if isPenalty(pos(i,:),extPar) % checks if each position is penalizable
-        penalty = penalty + calcPenalty(pos(i,:),extPar);
-        auxOutput(model).penalty = true; % sets penalized flag to true if even one position is penalized
-    end
-end
-
-transMatZ = calcTransMatZThird(pos);  %this matrix should be the truncated
-%order with no errors
-
-for i = 1:extPar.numAverages
+for n = extPar.numAverages:-1:1
     
+    pos = reshape(Xtemp,length(Xtemp)./3,3);
     posErrors = calcPosErrors(pos,extPar.posError);
+    penalty = 0;
+    auxOutput(model).penalty = false;
+    for i = 1:length(pos(:,1))
+        if isPenalty(pos(i,:),extPar) % checks if each position is penalizable
+            penalty = penalty + calcPenalty(pos(i,:),extPar);
+            auxOutput(model).penalty = true; % sets penalized flag to true if even one position is penalized
+        end
+    end
     
     g = genGradRandom();%generate random gradient on the order of the largest
     %expected from the measured uniformity of magnetically shielded rooms
@@ -26,18 +21,36 @@ for i = 1:extPar.numAverages
     dReal = calcdFalseFifth(extPar.R,extPar.h1,extPar.h2,g); %dReal should
     %always be calculated to the highest order with no errors
     
-    for j = length(posErrors):-1:1
-        Bfield(j,1) = calcBavg(extPar,posErrors(j,:),g) + randn.*extPar.fieldError + extPar.offset;
-    end
     
-    g_fit = calcGrad(Bfield,transMatZ);%truncated order, NO ERRORS IN TRANSMAT
+    
+    for i = length(pos):-1:1
+        
+        posTemp = pos;
+        posErrorsTemp = posErrors;
+        
+        posTemp(i,:) = [];
+        posErrorsTemp(i,:) = [];
+        
+        transMatZ = calcTransMatZThird(posTemp);  %this matrix should be the truncated
+        %order with no errors
+        
+        for j = length(posErrorsTemp):-1:1
+            Bfield(j,1) = calcBavg(extPar,posErrorsTemp(j,:),g) + randn.*extPar.fieldError + extPar.offset;
+        end
+        
+        g_fit(:,i) = calcGrad(Bfield,transMatZ);%truncated order, NO ERRORS IN TRANSMAT
+        
+        %fitness = 1e6*sqrt(sum((g(1:length(g_fit)) - g_fit).^2));
+        
+    end
+    g_fit = mean(g_fit,2);
     
     dFit = calcdFalseThird(extPar.R,extPar.h1,extPar.h2,g_fit); %truncated order
     
-    %fitness = 1e6*sqrt(sum((g(1:length(g_fit)) - g_fit).^2));
-    
-    fitness = fitness + penalty + 100*(abs(dReal - dFit))/abs(dReal);%goal is for this to be as
+    fitness(n) = penalty + abs(dReal - dFit);%goal is for this to be as
     %low as possible for any arbitrary g
 end
-fitness = fitness/extPar.numAverages;
+
+fitness = mean(fitness);
+
 end
